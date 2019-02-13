e2eb3c38ceb19544c9776919e220e8518b53e140
BJHDieben <bjhdieben@gmail.com>
2019-02-13 20:22:54 +0100

    changes 4

--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/PI_Pneumatics.cpp
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/PI_Pneumatics.cpp
@@ -1,52 +1,29 @@
-<<<<<<< HEAD
 #include "PI_Pneumatics.h"//include the .h file of PI_Pneumatics
 
-PI_Pneumatics::PI_Pneumatics(int one,int two, int re, int ex)
+PI_Pneumatics::PI_Pneumatics(int one,int two, int re, int ex) //constructore (controle pin one, controle pin two, pin for retraction endswitch, pin for extention endswitch)
 {
 int _one = one;
 int _two = two;
 int _ex = ex;
 int _re = re;
-=======
-#include <PI_Pneumatics.h> //include the .h file of PI_Pneumatics
-
-PI_Pneumatics::PI_Pneumatics(uint8_t fwd, uint8_t rev)
-{
-
-    //create the object:
-    this->piston = new frc::DoubleSolenoid(fwd, rev);
->>>>>>> f0e9bb8107631b7c1f0ae5e62eb79c7e8c217fa7
 }
 
 void PI_Pneumatics::OpenPiston() //function to fully open a piston
 {
-<<<<<<< HEAD
     frc::DoubleSolenoid(_one,_two).Set(frc::DoubleSolenoid::Value::kForward);//comand open piston
 }
 
 void PI_Pneumatics::ClosePiston()//function to fully close a piston
 {
     frc::DoubleSolenoid(_one,_two).Set(frc::DoubleSolenoid::Value::kReverse);//comand close piston
-=======
-    piston->Set(frc::DoubleSolenoid::Value::kForward); //comand open piston
 }
 
-void PI_Pneumatics::ClosePiston() //function to fully close a piston
+int PI_Pneumatics::PistonStatus()// returns the current status of the piston
 {
-    piston->Set(frc::DoubleSolenoid::Value::kReverse); //comand close piston
->>>>>>> f0e9bb8107631b7c1f0ae5e62eb79c7e8c217fa7
+    if (frc::DigitalInput(_ex).Get()) return 1;
+    if (frc::DigitalInput(_re).Get()) return 0;
+    else return 2;
 }
 
-bool PI_Pneumatics::PistonStatus()
-{
-<<<<<<< HEAD
-    if (frc::DigitalInput(_ex).Get()) return frc::DigitalInput(_ex).Get();
-    if (frc::DigitalInput(_re).Get()) return frc::DigitalInput(_re).Get();
-=======
-    //return the state of the solenoid:
-
-    return piston->Get();
->>>>>>> f0e9bb8107631b7c1f0ae5e62eb79c7e8c217fa7
-}
 
  
--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/include/PI_Pneumatics.h
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/include/PI_Pneumatics.h
@@ -8,42 +8,22 @@
  *
  */
 #pragma once
-<<<<<<< HEAD
 #include <WPILib.h> //include WPILib.h file
 #include <DoubleSolenoid.h> //include the DoubleSolenoid.h file
-#include <DigitalInput.h> 
-#include <PI_Pins.h>
-=======
-#include "frc/DoubleSolenoid.h"
->>>>>>> f0e9bb8107631b7c1f0ae5e62eb79c7e8c217fa7
+#include <DigitalInput.h> //include the DigitalInput.h file
 
 class PI_Pneumatics//make a class called PI_Pneumatics
 {
 private: 
-<<<<<<< HEAD
-int _one;
-int _two;
-int _ex;
-int _re;
+int _one;//pin one for controle
+int _two;//pin two for controle
+int _ex;//extendsion endswitch
+int _re;//retraction endswitch
 
 public:
 PI_Pneumatics(int one,int two, int re, int ex);
-=======
- frc::DoubleSolenoid *piston;
-
-public:
-
-/*
-    Constructor:    setup one piston controlled with a double solenoid
-    Input:          CAN pneumatics controller channels 
-    output:         none
-*/
-PI_Pneumatics(uint8_t fwd, uint8_t rev);
-
-
->>>>>>> f0e9bb8107631b7c1f0ae5e62eb79c7e8c217fa7
 void OpenPiston();//difine function for fully opening the piston
 void ClosePiston();//difine function for fully closing the piston
-bool PistonStatus();
+int PistonStatus(); // gives the status of the piston 0 is retrected 1 is extended 2 is neutral
 };
 
