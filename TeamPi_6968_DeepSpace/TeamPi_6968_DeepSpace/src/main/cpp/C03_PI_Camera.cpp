d39dbd39023f6dea54ac9293363aad34bb82c3f8
Nick Dubbeldam <nick.dubbeldam@live.nl>
2019-02-16 13:33:26 +0100

    update

--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/C03_PI_Camera.cpp
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/C03_PI_Camera.cpp
@@ -2,7 +2,6 @@
 #include "C03_PI_Camera.h"
 
 C03_PI_Camera::C03_PI_Camera(){
-    frc::CameraServer::GetInstance()->StartAutomaticCapture("cam0");
-    frc::CameraServer::GetServer()->StartAutomaticCapture("cam0");
+    frc::CameraServer::GetInstance()->StartAutomaticCapture(0);
 }
 
