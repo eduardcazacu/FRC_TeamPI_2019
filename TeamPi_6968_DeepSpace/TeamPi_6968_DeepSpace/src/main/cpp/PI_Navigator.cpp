34dd9ce2acca006ae9309291a5f8e316152ab704
JornTeamPi <jt.veenendaal@hotmail.com>
2019-02-17 19:00:10 +0100

    added mantual control seconde controler (gamepad)

--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/PI_Navigator.cpp
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/PI_Navigator.cpp
@@ -1,5 +1,46 @@
 #include "PI_Navigator.h"
 
-PI_Navigator::PI_Navigator()
+PI_Navigator::PI_Navigator(int ManualControllerID)
 {
+    this->*ManualController = new frc::Controller(ManualControllerID);
+    this->*gamePadPOV0 = new frc::POVButton(frc::GenericHID::kHIDGamepad, 270, 0);
+    this->*gamePadPOV1 = new frc::POVButton(frc::GenericHID::kHIDGamepad, 0, 0);
+    this->*gamePadPOV2 = new frc::POVButton(frc::GenericHID::kHIDGamepad, 360, 0);
+}
+bool PI_Navigator::climbUpBtn()
+{
+    this->*ManualController->
+}
+
+bool PI_Navigator::climbFrontDownBtn()
+{
+    return this->ManualController->GetXButtonPressed();
+}
+
+bool PI_Navigator::lastGripperExtendBtn()
+{
+    return this->ManualController->GetYButtonPressed();
+}
+
+bool PI_Navigator::gripperRetractBtn()
+{
+    return this->ManualController->GetAButtonPressed();
+}
+
+bool PI_Navigator::gripBtn()
+{
+    return this->ManualController->GetTriggerAxis(joystickHand::kRightHand);
+}
+double PI_Navigator::ManualLift()
+{
+    return this->ManualController->GetY(JoystickHand::kRightHand);
+}
+bool PI_Navigator::lvl0Btn()
+{
+    return this->ManualController->get
+}
+
+bool PI_Navigator::lvl0Btn90(){
+    return this->gamePadPOV0->Get();
+
 }
--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/include/PI_Navigator.h
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/include/PI_Navigator.h
@@ -8,11 +8,35 @@ Team pi 6968
 
 
 #include "PI_TargetNames.h"
+#include <XboxController.h>
+#include <frc/buttons/Button.h>
+
+
 
 class PI_Navigator
 {   
     public:
-        PI_Navigator();
+        PI_Navigator(uint8_t controllerID);
+        frc::XboxController *ManualController;
+        frc::POVButton *gamePadPOV0;
+        frc::POVButton *gamePadPOV1;
+        frc::POVButton *gamePadPOV2;
+
+        //manual climbing 
+        bool climbUpBtn();
+        bool climbFrontDownBtn();
+        bool climbBackDownBtn();
+        
+        // manual gripper
+        bool lastGripperExtendBtn();
+        bool gripperRetractBtn();
+        bool gripBtn();
+        //lift 
+        double  ManualLift();
+        bool lvl0Btn();
+        bool lvl1Btn();
+        bool lvl2Btn();
+
         AutoTarget target;
 
     private:
