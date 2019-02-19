88a604b7683450ca58fbb4ef427b15596fb475d5
Eduard Cazacu <eddy.cazacu@gmail.com>
2019-02-19 07:03:18 +0100

    positioning adjustment

--- TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/S03_PI_Positioning.cpp
+++ TeamPi_6968_DeepSpace/TeamPi_6968_DeepSpace/src/main/cpp/S03_PI_Positioning.cpp
@@ -20,8 +20,8 @@ S03_PI_Positioning::S03_PI_Positioning(S00_PI_Network *network, C00_PI_Talon *le
 
 void S03_PI_Positioning::refresh()
 {
-    double rDist = _rEnc->deltaDistance(); //one of the sides needs to be reversed
-    double lDist = -_lEnc->deltaDistance();
+    double rDist = -_rEnc->deltaDistance(); //one of the sides needs to be reversed
+    double lDist = _lEnc->deltaDistance();
 
     //find the distance
     double distance = (rDist - lDist) / 2 + lDist;
